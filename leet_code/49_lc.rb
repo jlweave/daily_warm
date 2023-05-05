eb186e7a97e0f1b0d70a805518abaa232a9e43cc
Lacey Weaver <jlweave89@gmail.com>
2023-05-05 09:29:45 -0600

    may 5

---
+++ leet_code/49_lc.rb
@@ -0,0 +1,29 @@
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+# Example 1:
+
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+# Example 2:
+
+# Input: strs = [""]
+# Output: [[""]]
+# Example 3:
+
+# Input: strs = ["a"]
+# Output: [["a"]]
+
+# @param {String[]} strs
+# @return {String[][]}
+
+strs = ["eat","tea","tan","ate","nat","bat"]
+
+def group_anagrams(strs)
+ strs.group_by { |s| s.chars.sort }.values
+end
+
+p group_anagrams(strs)
